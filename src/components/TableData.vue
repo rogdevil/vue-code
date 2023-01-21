3666ea027eac5a1a411bbbabb8d7446c634be1a3
rogdevil <sep2001aryan@yahoo.com>
2023-01-22 01:17:57 +0530

    prettier changes

--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -1,44 +1,19 @@
-<script
-	setup
-	lang="ts"
->
+<script setup lang="ts">
 import type { PropType } from "vue";
 
-const {
-	data,
-	isImage = false,
-} =
-	defineProps(
-		{
-			data: {
-				type: String,
-				required:
-					true,
-			},
-			isImage:
-				Boolean,
-		}
-	);
+const { data, isImage = false } = defineProps({
+	data: {
+		type: String,
+		required: true,
+	},
+	isImage: Boolean,
+});
 </script>
 <template>
-	<td
-		:width="
-			200
-		"
-	>
-		<img
-			v-if="
-				isImage
-			"
-			:src="(data as string)"
-			alt="data"
-		/>
-		<p
-			v-else
-		>
-			{{
-				data
-			}}
+	<td :width="200">
+		<img v-if="isImage" :src="(data as string)" alt="data" />
+		<p v-else>
+			{{ data }}
 		</p>
 	</td>
 </template>
--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,51 +1,24 @@
-<script
-	setup
-	lang="ts"
->
+<script setup lang="ts">
 import type { IUser } from "@/interfaces";
 import type { PropType } from "vue";
 import TableData from "./TableData.vue";
 
-const {
-	user,
-} =
-	defineProps(
-		{
-			user: {
-				type: Object as PropType<IUser>,
-				required:
-					false,
-			},
-		}
-	);
+const { user } = defineProps({
+	user: {
+		type: Object as PropType<IUser>,
+		required: false,
+	},
+});
 </script>
 <template>
 	<tr>
-		<slot
-			:data="
-				user
-			"
-		>
-		</slot>
+		<slot :data="user"> </slot>
 		<TableData
-			v-if="
-				user
-			"
-			v-for="(
-				item,
-				key,
-				index
-			) in user"
-			:data="
-				item
-			"
-			:key="
-				user.email
-			"
-			:is-image="
-				key ===
-				'picture'
-			"
+			v-if="user"
+			v-for="(item, key, index) in user"
+			:data="item"
+			:key="user.email"
+			:is-image="key === 'picture'"
 		></TableData>
 	</tr>
 </template>
